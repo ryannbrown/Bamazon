11ecad49e282b1175c1667bf1775dbc20f776e9d
ryannbrown <44414542+ryannbrown@users.noreply.github.com>
2019-05-07 12:45:34 -0400

    Added js file with inquirer and mysql

---
+++ bamazonCustomer.js
@@ -0,0 +1,34 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "Soccer2121!",
+  database: "bamazon"
+});
+
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+  if (err) throw err;
+  // run the start function after the connection is made to prompt the user
+  displayProducts();
+});
+
+
+function displayProducts() {
+    connection.query("SELECT * FROM products", function(err, res) {
+      for (var i = 0; i < res.length; i++) {
+        console.log(res[i].id + " | " + res[i].title + " | " + res[i].artist + " | " + res[i].genre);
+      }
+      console.log("-----------------------------------");
+    });
+  }
