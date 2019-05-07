4558fbd16cda705e47cbe0ee18eca4b58ec68ddb
ryannbrown <44414542+ryannbrown@users.noreply.github.com>
2019-05-07 13:06:57 -0400

    Can now successfully display inventory

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -27,8 +27,19 @@ connection.connect(function(err) {
 function displayProducts() {
     connection.query("SELECT * FROM products", function(err, res) {
       for (var i = 0; i < res.length; i++) {
-        console.log(res[i].id + " | " + res[i].title + " | " + res[i].artist + " | " + res[i].genre);
+        console.log(res[i].id + " | " + res[i].product_name + " | " + res[i].department_name + " | $" +  res[i].price + " | " + res[i].stock_quantity);
       }
       console.log("-----------------------------------");
     });
+    requestOrder()
+  }
+
+
+
+  function requestOrder(){
+
+  }
+
+  function placeOrder(){
+
   }
